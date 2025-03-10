f12bc468e5e73a5c3beb8de5d4407484a8500cb0
Rahul Ranjan <rr842001@gmail.com>
2025-03-10 11:00:26 +0530

    fix: collect all logs

--- next.config.mjs
+++ next.config.mjs
@@ -20,11 +20,6 @@ const nextConfig = {
     webpackBuildWorker: true,
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
-  },
-  compiler:{
-    removeConsole: process.env.NODE_ENV === 'production' ? {
-      exclude: ['error'],
-    } : false
   }
 }
 
